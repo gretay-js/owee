b76c465b99db1cab49ce351d2fc7cbf5e323aec9
Greta Yorsh <gyorsh@janestreet.com>
2019-03-27 19:08:31 +0000

    Fix compilation errors with ocaml-4.07.1

--- examples/decodedline_elf.ml
+++ examples/decodedline_elf.ml
@@ -11,8 +11,13 @@ let path =
 let buffer =
   let fd = Unix.openfile path [Unix.O_RDONLY] 0 in
   let len = Unix.lseek fd 0 Unix.SEEK_END in
-  let map = Bigarray.Array1.map_file fd
-      Bigarray.int8_unsigned Bigarray.c_layout false len in
+  let map = Bigarray.array1_of_genarray
+              (Unix.map_file
+                 fd
+                 Bigarray.int8_unsigned
+                 Bigarray.c_layout
+                 false
+                 [| len |]) in
   Unix.close fd;
   map
 
